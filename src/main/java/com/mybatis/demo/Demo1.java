069123cb6bfae4f461a0e2b6cf79aa189a387a20
xuxy <xuxy@fengyuntec.com>
2018-11-19 17:26:42 +0800

    learn mybatis

---
+++ src/main/java/com/mybatis/demo/Demo1.java
@@ -0,0 +1,14 @@
+package com.mybatis.demo;
+
+import org.springframework.jdbc.support.JdbcUtils;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+public class Demo1 {
+    public static void main(String[] args){
+        Connection con = null;
+        PreparedStatement preparedStatement = null;
+//        con = JdbcUtils.
+    }
+}
